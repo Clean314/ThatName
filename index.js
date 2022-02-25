1d9da3fe2c4fe7f0e408988fd3cc3f181c7de86d
Pipie314 <pipie314@protonmail.com>
2022-02-26 01:28:05 +0900

    퀴즈 스키마 + 퀴즈 목록 & 추가 페이지

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 //express, mongoose, ejs, body parser 초기화
 ﻿var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
+var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
 
-app.set('view engine','ejs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+var tCounter = 0;
+var multer = require('multer');
+var upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/quiz_image/');
+    },
+    filename: function (req, file, cb) {
+      ext = (file.originalname).split(".");
+      cb(null, tCounter + "." + ext[ext.length - 1]);
+    }
+  })
+});
 
 //DB셋팅
 dburl = "mongodb+srv://ThatNameUser:ThatNamePassword@thatnamedb.wt7qc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -19,9 +30,28 @@ db.on('error', function(err){
   console.log('DB ERROR : ', err);
 });
 
-//static 경로 설정
+app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+var quizSchema = mongoose.Schema({
+  Pid : {type:Number, required:true, unique:true},
+  answer : {type:Array},
+  comment : {type:String},
+  ext : {type:String}
+});
+var Quiz = mongoose.model('quiz', quizSchema);
+var tCounter = 0;
+Quiz.countDocuments({}, function (err, count) {
+    if (err){
+        console.log(err);
+    }else{
+        tCounter = count;
+    }
+});
+
+//CONTENT
 //content를 루트로
 app.get('/', function(req, res){
   res.redirect('/content');
@@ -35,6 +65,34 @@ app.get('/content', function(req, res){
 //quiz_page로 get 시에 quiz_page.ejs
 app.get('/content/quiz_page', function(req, res){
   res.render('content/quiz_page');
+});
+
+//ADMIN
+//quiz_list로 Get 시에
+app.get('/admin/quiz_list', function(req, res){
+  Quiz.find({}, function(err, quiz){
+    if(err) return res.json(err);
+    res.render('admin/quiz_list', {quiz_key:quiz});
+  });
+});
+
+// quiz_list로 Post 시에
+app.post('/admin/quiz_list', upload.single('image'), function(req, res){
+  req.body.Pid = tCounter;
+  req.body.answer = ((String)(req.body.answer)).split(",");
+  ext = ((String)(req.file.originalname)).split(".");
+  req.body.ext = ext[ext.length - 1];
+
+  console.log(req.body);
+  Quiz.create(req.body, function(err, quiz){
+    if(err) return res.json(err);
+  });
+  res.render('admin/quiz_list');
+});
+
+//quiz_add로 Get 시에
+app.get('/admin/quiz_add', function(req, res){
+  res.render('admin/quiz_add');
 });
 
 var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
